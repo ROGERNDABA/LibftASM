e92b6842e82eff0ede623be329b4338dcc3c3f5f
roger NDABA <rmdaba@c4r17s11.wethinkcode.co.za>
2019-08-20 16:23:17 +0200

    edit: run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-if [ "$#" -eq  "0" ]
-    then
-    echo "No arguments supplied"
-else
-    for i in "$@"; do
-        nasm -fmacho64 $i.s
-    done
-    gcc object_t.c main.c srcs/*.o
-fi
+gcc object_t.c main.c obj/*.o

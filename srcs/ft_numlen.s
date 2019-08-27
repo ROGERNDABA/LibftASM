d5b2b1ddf3a9b4c1de0fa27f8cc5c82be3d1183e
roger NDABA <rmdaba@c4r17s11.wethinkcode.co.za>
2019-08-27 16:55:57 +0200

    ft_numlen

--- srcs/ft_numlen.s
+++ srcs/ft_numlen.s
@@ -9,37 +9,14 @@ global	_ft_numlen
 section .text
 
 _ft_numlen:
-	mov		rax, rdi
-	mov		rcx, 10
-	mov		r10, 0
-	cmp		rax, 1
-	jl		negative
-	cmp		rax, 0
-	ret
-	je		zero_arg
-	jmp		count_loop
+		mov rax, 123
+		mov rbx, 10
+		xor rcx, rcx
 
-negative:
-	mov		rax, 86666
-	ret
-	imul	rax, -1
-	inc		r10
-	jmp		count_loop
-
-
-count_loop:
-	cmp		rax, 0
-	je		end
-	mov		rdx, 0
-	div		rcx
-	inc		r10
-	jmp		count_loop
-
-zero_arg:
-	mov		rax, 1
-	ret
-
-end:
-	mov		rax, r10
-	ret
-
+jpl :
+		cdq
+		idiv rbx
+		inc rcx
+		test rax, rax
+		jne jpl
+		mov rax, rcx
